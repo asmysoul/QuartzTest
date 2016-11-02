8b95e3072723924c15d0c7377e7c3221b5d30712
asmysoul <27708324@qq.com>
2016-11-02 17:29:20 +0800

    修改部分数据

---
+++ src/main/java/top/fzqblog/quartz/job/MyJob.java
@@ -0,0 +1,54 @@
+/******************************************************************
+ *
+ *    
+ *
+ *    Copyright (c) 2016-forever 
+ *    http://www.fzqblog.top
+ *
+ *    Package:     top.fzqblog.job
+ *
+ *    Filename:    MyJob.java
+ *
+ *    Description: TODO(用一句话描述该文件做什么)
+ *
+ *    Copyright:   Copyright (c) 2001-2014
+ *
+ *    Company:     fzqblog
+ *
+ *    @author:     抽离
+ *
+ *    @version:    1.0.0
+ *
+ *    Create at:   2016年11月2日 下午4:36:10
+ *
+ *    Revision:
+ *
+ *    2016年11月2日 下午4:36:10
+ *        - first revision
+ *
+ *****************************************************************/
+package top.fzqblog.quartz.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * @ClassName MyJob
+ * @Description TODO(这里用一句话描述这个类的作用)
+ * @author 抽离
+ * @Date 2016年11月2日 下午4:36:10
+ * @version 1.0.0
+ */
+public class MyJob implements Job{
+
+	/* (非 Javadoc)
+	 * Description:
+	 * @see org.quartz.Job#execute(org.quartz.JobExecutionContext)
+	 */
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		
+		
+	}
+
+}
--- src/main/java/top/fzqblog/quartz/trigger/CronTriggerManager.java
+++ src/main/java/top/fzqblog/quartz/trigger/CronTriggerManager.java
@@ -5,9 +5,9 @@
  *    Copyright (c) 2016-forever 
  *    http://www.fzqblog.top
  *
- *    Package:     top.fzqblog.job
+ *    Package:     top.fzqblog.quartz.trigger
  *
- *    Filename:    MyJob.java
+ *    Filename:    CronTriggerManager.java
  *
  *    Description: TODO(用一句话描述该文件做什么)
  *
@@ -19,36 +19,23 @@
  *
  *    @version:    1.0.0
  *
- *    Create at:   2016年11月2日 下午4:36:10
+ *    Create at:   2016年11月2日 下午5:02:35
  *
  *    Revision:
  *
- *    2016年11月2日 下午4:36:10
+ *    2016年11月2日 下午5:02:35
  *        - first revision
  *
  *****************************************************************/
-package top.fzqblog.job;
-
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+package top.fzqblog.quartz.trigger;
 
 /**
- * @ClassName MyJob
+ * @ClassName CronTriggerManager
  * @Description TODO(这里用一句话描述这个类的作用)
  * @author 抽离
- * @Date 2016年11月2日 下午4:36:10
+ * @Date 2016年11月2日 下午5:02:35
  * @version 1.0.0
  */
-public class MyJob implements Job{
-
-	/* (非 Javadoc)
-	 * Description:
-	 * @see org.quartz.Job#execute(org.quartz.JobExecutionContext)
-	 */
-	public void execute(JobExecutionContext context) throws JobExecutionException {
-	
-		
-	}
+public class CronTriggerManager {
 
 }
