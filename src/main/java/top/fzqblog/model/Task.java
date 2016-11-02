3dfcb4a1bb26e44e029ba5306339e3c21490cb0b
asmysoul <27708324@qq.com>
2016-11-02 16:53:53 +0800

    定时任务

---
+++ src/main/java/top/fzqblog/model/Task.java
@@ -0,0 +1,41 @@
+/******************************************************************
+ *
+ *    
+ *
+ *    Copyright (c) 2016-forever 
+ *    http://www.fzqblog.top
+ *
+ *    Package:     top.fzqblog.model
+ *
+ *    Filename:    Task.java
+ *
+ *    Description: TODO(用一句话描述该文件做什么)
+ *
+ *    Copyright:   Copyright (c) 2001-2014
+ *
+ *    Company:     fzqblog
+ *
+ *    @author:     抽离
+ *
+ *    @version:    1.0.0
+ *
+ *    Create at:   2016年11月2日 下午4:51:27
+ *
+ *    Revision:
+ *
+ *    2016年11月2日 下午4:51:27
+ *        - first revision
+ *
+ *****************************************************************/
+package top.fzqblog.model;
+
+/**
+ * @ClassName Task
+ * @Description TODO(这里用一句话描述这个类的作用)
+ * @author 抽离
+ * @Date 2016年11月2日 下午4:51:27
+ * @version 1.0.0
+ */
+public class Task {
+	
+}
